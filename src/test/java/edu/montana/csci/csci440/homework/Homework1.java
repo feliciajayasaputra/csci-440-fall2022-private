a78effe1c05cfb1683b9be539ad73e7f77a0bdef
Jayasaputra <b81p263@msu.montana.edu>
2022-09-28 11:29:41 -0600

    Start Homework 1

--- src/test/java/edu/montana/csci/csci440/homework/Homework1.java
+++ src/test/java/edu/montana/csci/csci440/homework/Homework1.java
@@ -14,7 +14,7 @@ public class Homework1 extends DBTest {
      * Write a query in the string below that returns all artists that have an 'A' in their name
      */
     void selectArtistsWhoseNameHasAnAInIt(){
-        List<Map<String, Object>> results = executeSQL("SELECT * FROM artists");
+        List<Map<String, Object>> results = executeSQL("SELECT * FROM artists WHERE name LIKE '%A%'");
         assertEquals(211, results.size());
     }
 
@@ -24,7 +24,7 @@ public class Homework1 extends DBTest {
      */
     void selectAllArtistsWithMoreThanOneAlbum(){
         List<Map<String, Object>> results = executeSQL(
-                "SELECT * FROM artists");
+                "SELECT * FROM artists JOIN albums ON artists.ArtistsId = albums.AlbumsId WHERE albums > 1 ");
 
         assertEquals(56, results.size());
         assertEquals("AC/DC", results.get(0).get("Name"));
@@ -38,7 +38,9 @@ public class Homework1 extends DBTest {
     void selectTheTrackAndAlbumAndArtistForAllTracksLongerThanSixMinutes() {
         List<Map<String, Object>> results = executeSQL(
                 "SELECT tracks.Name as TrackName, albums.Title as AlbumTitle, artists.Name as ArtistsName FROM tracks " +
-                        "-- NEED TO DO SOME JOINS HERE KIDS");
+                        "JOIN albums ON tracks.AlbumId = albums.AlbumId " +
+                        "JOIN artists ON albums.ArtistId = artists.ArtistId" +
+                        "WHERE tracks.milliseconds > 360000");
 
         assertEquals(623, results.size());
 
